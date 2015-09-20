732c951cf412c8fab0970cdce16cc6ac43ed1438
hmelberg <hans.melberg@gmail.com>
2015-09-21 01:11:50 +0200

    updated notebook and more pypi test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'motionchart',
+  packages = ['motionchart'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A wrapper to make motion charts from pandas dataframes',
+  author = 'Hans Olav Melberg',
+  author_email = 'hans.melberg@gmail.com',
+  url = 'https://github.com/hmelberg/motionchart', # use the URL to the github repo
+  download_url = 'https://github.com/hmelberg/motionchart/tarball/0.1', # I'll explain this in a second
+  keywords = ['motion chart', 'pandas', 'visualization'], # arbitrary keywords
+  classifiers = [],
+)
