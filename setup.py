763ec4400d1f87a5ec61bfd196fa622c27219d4e
hmelberg <hans.melberg@gmail.com>
2015-09-22 01:24:40 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'motionchart',
   packages = ['motionchart'], # this must be the same as the name above
   version = '0.1',
-  description = 'A wrapper to make motion charts from pandas dataframes',
+  description = 'A wrapper to make interactive motion chart visualizations from pandas dataframes',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
   url = 'https://github.com/hmelberg/motionchart', # use the URL to the github repo
