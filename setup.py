cd4ecdd0fc281455336dc7a07441dbb6f2c26c46
hmelberg <hans.melberg@gmail.com>
2015-09-25 22:44:16 +0200

    removed install_requires from setup.py
    install_requires

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 setup(
   name = 'motionchart',
   packages = ['motionchart'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A wrapper to make interactive motion chart visualizations from pandas dataframes',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
   url = 'https://github.com/hmelberg/motionchart', # use the URL to the github repo
-  download_url = 'https://github.com/hmelberg/motionchart/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/hmelberg/motionchart/tarball/0.2', # I'll explain this in a second
   keywords = ['motion chart', 'pandas', 'visualization'], # arbitrary keywords
-  classifiers = [],
-  install_requires = ['webbrowser', 'pandas', 'pyperclip']
+  classifiers = []
 )
