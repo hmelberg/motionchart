14e62e220eadadbc1fe7d465bab2a002764517d7
hmelberg <hans.melberg@gmail.com>
2015-10-17 23:58:43 +0200

    update to 0.3

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'motionchart',
-  packages = ['motionchart'], # this must be the same as the name above
-  version = '0.2',
+  packages = ['motionchart'], 
+  version = '0.3',
   description = 'A wrapper to make interactive motion chart visualizations from pandas dataframes',
   author = 'Hans Olav Melberg',
   author_email = 'hans.melberg@gmail.com',
-  url = 'https://github.com/hmelberg/motionchart', # use the URL to the github repo
-  download_url = 'https://github.com/hmelberg/motionchart/tarball/0.2', # I'll explain this in a second
-  keywords = ['motion chart', 'pandas', 'visualization'], # arbitrary keywords
+  url = 'https://github.com/hmelberg/motionchart', 
+  download_url = 'https://github.com/hmelberg/motionchart/tarball/0.3', 
+  keywords = ['motion chart', 'pandas', 'visualization'], 
   classifiers = []
 )
